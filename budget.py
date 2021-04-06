c5ba3365e1a10ad7fdd2a5cce1725a771097b3f3
Zachary Sparks <>
2021-04-06 21:49:08 +0000

    Create methods for future use.

--- budget.py
+++ budget.py
@@ -1,7 +1,17 @@
 class Category:
+  def __init__(self, category):
+    self.category = category
 
-
-
-
+  def deposit(self, amount, description=False):
+    return ""
+  def withdraw(self, amount, description=False):
+    return ""
+  def get_balance(self):
+    return ""
+  def transfer(self, amount, category):
+    return ""
+  def check_funds(self, amount):
+    return ""
 
 def create_spend_chart(categories):
+  return "5"
